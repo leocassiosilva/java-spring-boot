91e2bd97beed02413662b363e54fb6415156ea59
leocassiosilva <leocassiosilva1234@gmail.com>
2025-05-20 20:09:29 -0300

    0603 Retornando Listas de Objetos em JSON

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -3,6 +3,9 @@ package br.com.erudio.controllers;
 
 import br.com.erudio.model.Person;
 import br.com.erudio.request.converters.PersonServices;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +20,12 @@ public class PersonController {
 
     @Autowired
     private PersonServices service;
+    @RequestMapping(method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll(){
+        return service.findAll();
+    }
 
     @RequestMapping(value = "/{id}",
             method = RequestMethod.GET,
@@ -25,4 +34,9 @@ public class PersonController {
     public Person findById(@PathVariable("id") String id){
         return service.findById(id);
     }
+
+
+
+
+
 }
--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/request/converters/PersonServices.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/request/converters/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.erudio.request.converters;
 import br.com.erudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,20 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+
+    public List<Person> findAll(){ 
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+
+    }
+
+
 
     public Person findById(String id){
         logger.info("Finding one Person!");
@@ -24,5 +40,16 @@ public class PersonServices {
         return person;
 
 
+    }
+
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("First Name " + i);
+        person.setLastName("Last Name " + i);
+        person.setAddress("Some Andress in Brasil");
+        person.setGender("Male");
+        return person;
     }
 }
