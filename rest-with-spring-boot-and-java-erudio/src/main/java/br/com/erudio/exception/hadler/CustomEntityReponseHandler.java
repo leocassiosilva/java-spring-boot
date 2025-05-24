549ca08816ce7f22285aa8d5b6bb124b2de8115a
leocassiosilva <leocassiosilva1234@gmail.com>
2025-05-24 11:51:44 -0300

    0705 Substituindo os Mocks do Service pelo Acesso ao Repositório

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exception/hadler/CustomEntityReponseHandler.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exception/hadler/CustomEntityReponseHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.erudio.exception.ExceptionResponse;
-import br.com.erudio.exception.UnsupportedMathOperationException;
+import br.com.erudio.exception.ResourceNotFoundException;
 
 
 @ControllerAdvice
@@ -34,7 +34,7 @@ public class CustomEntityReponseHandler extends ResponseEntityExceptionHandler {
 
     }
 
-    @ExceptionHandler(UnsupportedMathOperationException.class)
+    @ExceptionHandler(ResourceNotFoundException.class)
     public final ResponseEntity<ExceptionResponse> handleBadRequestException(Exception ex, WebRequest request) {
         ExceptionResponse response = new ExceptionResponse(
             new Date(), 
